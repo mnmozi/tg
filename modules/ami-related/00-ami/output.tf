208560b1b0f7bf84331cf0518363ba682b476928
Mostafa Nasr <mnmostafa.nasr@gmail.com>
2025-01-27 13:12:57 +0400

    fix: fixing output names

--- modules/ami-related/00-ami/output.tf
+++ modules/ami-related/00-ami/output.tf
@@ -1,30 +1,18 @@
 
 output "id" {
   description = "The ami id"
-  value       = aws_ami_from_instance.solr-ami.id
+  value       = aws_ami_from_instance.ami.id
 }
 
 output "ami" {
   description = "The ami id"
-  value       = aws_ami_from_instance.solr-ami
+  value       = aws_ami_from_instance.ami
 }
 output "name" {
   description = "The ami id"
-  value       = aws_ami_from_instance.solr-ami.name
+  value       = aws_ami_from_instance.ami.name
 }
 
-output "sdf_ebs_block_device" {
-  value = tomap({
-    for ebs in aws_ami_from_instance.solr-ami.ebs_block_device :
-    ebs.device_name => ebs
-    if ebs.device_name == "/dev/sdf"
-  })["/dev/sdf"]
-}
-
-output "sdg_ebs_block_device" {
-  value = tomap({
-    for ebs in aws_ami_from_instance.solr-ami.ebs_block_device :
-    ebs.device_name => ebs
-    if ebs.device_name == "/dev/sdg"
-  })["/dev/sdg"]
+output "ebs_block_device" {
+  value = aws_ami_from_instance.ami.ebs_block_device
 }
